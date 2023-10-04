097b70b03d20c7bad4d82d7e964c83fa941d8bad
rizafdian <afdianriz@gmail.com>
2023-10-04 13:15:10 +0700

    update

--- application/views/template_adminpa.php
+++ application/views/template_adminpa.php
@@ -39,9 +39,9 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
+      <!-- <li class="nav-item d-none d-sm-inline-block">
         <a href="<?=base_url()?>assets/template/index3.html" class="nav-link">Home</a>
-      </li>
+      </li> -->
       <li class="nav-item d-none d-sm-inline-block">
         <a href="<?php echo site_url('auth/logout/') ?>" class="nav-link">Logout</a> 
       </li>
@@ -65,7 +65,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?=base_url()?>assets/template/index3.html" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="<?=base_url()?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Admin Awass</span>
     </a>
