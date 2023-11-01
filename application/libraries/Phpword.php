90a2791fc6fc417c3a6112195326b77132628bc8
riyanboyuhu <riyanboyuhu@gmail.com>
2023-11-01 15:23:14 +0800

    merubah path

--- application/libraries/Phpword.php
+++ application/libraries/Phpword.php
@@ -1,12 +1,14 @@
 <?php
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once  APPPATH . 'third_party\PhpWord\Autoloader.php';
+require_once  APPPATH . 'third_party/PhpWord/Autoloader.php';
+
 use PhpOffice\PhpWord\Autoloader;
 use PhpOffice\PhpWord\Settings;
+
 Autoloader::register();
 Settings::loadConfig();
 
-class Phpword extends Autoloader {
-
+class Phpword extends Autoloader
+{
 }
