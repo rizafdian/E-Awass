df3de67820531be38a496cbde8513332b6044446
riyanboyuhu <riyanboyuhu@gmail.com>
2023-11-01 15:48:20 +0800

    ubah librari dari requireonce jadi require

--- application/libraries/Phpword.php
+++ application/libraries/Phpword.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once  APPPATH . 'third_party/PhpWord/Autoloader.php';
+require  APPPATH . 'third_party/PhpWord/Autoloader.php';
 
 use PhpOffice\PhpWord\Autoloader;
 use PhpOffice\PhpWord\Settings;
