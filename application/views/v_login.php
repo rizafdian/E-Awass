c2330854594dbbd09415fb4874d9fb9984f02e19
riyanboyuhu <riyanboyuhu@gmail.com>
2024-05-16 08:37:07 +0800

    rubah title dari awas ke APIK

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login Awass</title>
+	<title>Login APIK</title>
 	<link rel="icon" href="<?= base_url() ?>assets/template/dist/img/AdminLTELogo.png" type="image/x-icon" />
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
